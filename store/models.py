1d5ded914b49b64ea8db0b32ce081399a8d63006
bogdanminne <bogdannsdap@gmail.com>
2023-03-08 12:13:27 +0200

    Added admin panel with some products

--- store/models.py
+++ store/models.py
@@ -7,6 +7,11 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='category', blank=True)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__ (self):
         return self.name
     
@@ -21,6 +26,11 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return self.name
