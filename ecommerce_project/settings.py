d777a9deb4425691ccbf73a7224e260334d5cc1d
bogdanminne <bogdannsdap@gmail.com>
2023-03-09 10:53:22 +0200

    Display Categories in Navbar
    Added dynamic display of categories in the navbar section. Displays all products and individual ones as well.

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.context_processors.menu_links',
             ],
         },
     },
