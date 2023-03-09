46d07c37b765d4f626b1b1f2b6e4ce4f02dc537a
bogdanminne <bogdannsdap@gmail.com>
2023-03-09 11:07:24 +0200

    Display Product in Product Detail Template
    Adding functionality for displaying an individual product and description once you click on it.

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.productPage, name="product"),
+    path('<slug:category_slug>/<slug:product_slug>', views.productPage, name="product_detail"),
 ]
 
