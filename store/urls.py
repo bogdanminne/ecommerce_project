1eb26b4a932362e6ae0ff62993c490d44e4e39a4
bogdanminne <bogdannsdap@gmail.com>
2023-03-09 13:46:31 +0200

    Implementing "add to cart" logic
    Adding dynamic functionality for the buttons.

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('category/<slug:category_slug>', views.home, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name="product_detail"),
-    path('cart', views.cart, name='cart'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart', views.cart_detail, name='cart_detail'),
 ]
 
