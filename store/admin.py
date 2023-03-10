8cf6c3a06c9cc9bdac7e7a3bc82a7e3653b0bbd0
bogdanminne <bogdannsdap@gmail.com>
2023-03-10 09:55:58 +0200

    Customize Admin Display - can see and edit information about products like price, stock, and availability in the admin page

--- store/admin.py
+++ store/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Category, Product
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+    list_per_page = 20
+
+admin.site.register(Product, ProductAdmin)
